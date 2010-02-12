21f261a7ef58eaf757ac65b7c80022a12ff140d2
flekst <flekst@6dc8e420-1583-11df-a3d4-49c8ad8c1f61>
2010-02-12 00:35:18 +0000

    Recode config file Implement autocreation directoryes Implement less folders usage (no master avaible, only work & delta)

--- trunk/template/squash_it.sh
+++ trunk/template/squash_it.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 source ./fs.conf
-#echo $MASTER_SQUASH
 
-if [ -z $1 ] ; then
-    echo select filename for new  squash-file
+if [ -z "$MASTER_SQUASH" ] ; then
+    echo Configure ./fs.conf, please
     exit
 fi
 
-if [ -z $MASTER_SQUASH ] \
-    || [ -z $COMMON_DIR ] \
-    || [ -z $WORK_COMMON_MOUNTPOINT ] ; then
-    echo Configure ./fs.conf, please
-
-else
-    mount -o loop -t squashfs $MASTER_SQUASH ./master
-#    mount -o bind $COMMON_DIR ./common
-    mount -t aufs none -o br:./diff.work/=rw:./master/=ro ./work
-#    mount -t aufs none -o br:./diff.common=rw:./common=ro ./work${WORK_COMMON_MOUNTPOINT}
-#    mount -t proc none ./work/proc
-#    mount -o bind /dev/ ./work/dev
-    cd work
-    mksquashfs . $@
-    umount ./work
-    umount ./master
-
+if [ -z $1 ] ; then
+    echo select filename and other parameters for new  squash-file
+    echo usage: $0 /path/file-name.squash [--mksquashfs-params]
+    exit
 fi
+
+    #master
+    echo "mount master squash"
+    mount -o loop -t squashfs "$MASTER_SQUASH" "$WORK_DIR"
+    #master + delta
+    echo "mount master squash + delta via aufs"
+    mount -t aufs none -o "br:$WORK_DIR_DELTA/=rw:$WORK_DIR/=ro" "$WORK_DIR"
+
+    cd "$WORK_DIR"
+    mksquashfs . $@
+    echo "umount master squash + delta via aufs"
+    umount "$WORK_DIR"
+    echo "umount master squash"
+    umount "$WORK_DIR"
--- trunk/template/umount_it.sh
+++ trunk/template/umount_it.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 source ./fs.conf
-umount ./work/dev
-umount ./work/proc
-umount ./work${WORK_COMMON_MOUNTPOINT}
-umount ./work
-umount ./common
-umount ./master
+#echo $MASTER_SQUASH
+if [ -z $MASTER_SQUASH ] \
+    || [ -z $COMMON_DIR ] \
+    || [ -z $COMMON_DIR_MOUNTPOINT ] ; then
+    echo Configure ./fs.conf, please
+
+else
+    echo unmounting dev
+    umount $WORK_DIR/dev
+    echo unmounting proc
+    umount $WORK_DIR/proc
+    echo unmounting common dir
+    umount $WORK_DIR$COMMON_DIR_MOUNTPOINT
+    echo unmounting workdir squash+aufs
+    umount $WORK_DIR
+    echo unmounting workdir squash
+    umount $WORK_DIR
+fi
