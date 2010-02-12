d94c0a73eb6243c0b3d57ef45aedc4e33bc4b831
flekst <flekst@6dc8e420-1583-11df-a3d4-49c8ad8c1f61>
2010-02-12 00:35:18 +0000

    Recode config file Implement autocreation directoryes Implement less folders usage (no master avaible, only work & delta)

--- template/mount_it.sh
+++ template/mount_it.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 source ./fs.conf
 #echo $MASTER_SQUASH
-if [ -z $MASTER_SQUASH ] \
-    || [ -z $COMMON_DIR ] \
-    || [ -z $WORK_COMMON_MOUNTPOINT ] ; then
-    echo Configure ./fs.conf, please
-
-else
-    mount -o loop -t squashfs $MASTER_SQUASH ./master
-    mount -o bind $COMMON_DIR ./common
-    mount -t aufs none -o br:./diff.work/=rw:./master/=ro ./work
-    mount -t aufs none -o br:./diff.common=rw:./common=ro ./work${WORK_COMMON_MOUNTPOINT}
-    mount -t proc none ./work/proc
-    mount -o bind /dev/ ./work/dev
+if [ -z "$MASTER_SQUASH" ] \
+    || [ -z "$COMMON_DIR" ] \
+    || [ -z "$COMMON_DIR_MOUNTPOINT" ] ; then
+    echo "Configure ./fs.conf, please"
+    exit
 fi
+    #master
+    echo "mount master squash"
+    mount -o loop -t squashfs "$MASTER_SQUASH" "$WORK_DIR"
+    #master + delta
+    echo "mount master squash + delta via aufs"
+    mount -t aufs none -o "br:$WORK_DIR_DELTA/=rw:$WORK_DIR/=ro"  "$WORK_DIR"
+
+    echo "mount common dir"
+    #common + delta -> master.common
+    if ! [ -d  "$WORK_DIR$COMMON_DIR_MOUNTPOINT" ] ; then
+        mkdir -p "$WORK_DIR$COMMON_DIR_MOUNTPOINT"
+    fi
+    mount -t aufs none \
+    -o "br:$COMMON_DIR_DELTA/=rw:$COMMON_DIR=ro"\
+    "$WORK_DIR$COMMON_DIR_MOUNTPOINT"
+
+    #dev & proc
+    echo "mount proc & dev"
+    mount -t proc none 	"$WORK_DIR/proc"
+    mount -o bind /dev/ "$WORK_DIR/dev"
